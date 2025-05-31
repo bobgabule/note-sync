16e430c1c3a3b9daf9c95b9faf5440ca7f8aafd0
bobits <bobkeri.gabule@gmail.com>
2025-05-31 17:53:28 +0800

    review section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Page from "./components/sections/page";
 import Header from "./components/sections/Header";
 import Navigation from "./components/sections/Navigation";
 import Hero from "./components/sections/Hero";
+import Reviews from "./components/sections/Reviews";
 
 function App() {
   return <Page>
     <Header>
       <Navigation />
       <Hero />
+      <Reviews />
     </Header>
   </Page>;
 }
